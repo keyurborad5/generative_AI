7918bfb8336675d614b5061250dac96ead9be1e1
keyurborad5 <keyurborad5@gmail.com>
2025-06-29 16:08:36 -0400

    Added content in main.py  - Added tonkenization script in main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("hello world, this is a sample Python script.")
+# Importing a function from torchtext
+from torchtext.data.utils import get_tokenizer
+# Using the get_tokenizer function to tokenize a sample text
+#  The tokenizer is set to "basic_english" which is a simple tokenizer that splits text into words.
+# tokenizer = get_tokenizer("basic_english")
+# get_tokenizer (which is a function ) returns a callable function (here named tokenizer)
+# This tokenizer function can be used to tokenize text
+# tokenizer("This is a sample text for tokenization.")
